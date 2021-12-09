872e232e4259b98ecac57b364fe919b0237cebf5
Ian <egian93@gmail.com>
2021-12-08 20:04:59 -0600

    public folder with login.js created

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: 'Post',
+    freezeTableName: true,
+  }
+);
+
+module.exports = Post;
