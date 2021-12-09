aef9ced938b5edbee2b1b996b2048ef68d5c500c
Ian <egian93@gmail.com>
2021-12-08 20:02:50 -0600

    public folder with logout.js created

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    // If successfully logged out, redirect to the login page
+    document.location.replace('/login');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
